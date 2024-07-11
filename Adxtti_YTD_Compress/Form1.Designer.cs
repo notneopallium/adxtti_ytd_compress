d2d74acafd901eed231896e440129d403a40de30
neopallium <108044510+notneopallium@users.noreply.github.com>
2024-07-10 20:02:42 -0400

    update resx, designer class, and caching

--- Adxtti_YTD_Compress/Form1.Designer.cs
+++ Adxtti_YTD_Compress/Form1.Designer.cs
@@ -29,10 +29,19 @@ namespace Adxtti_YTD_Compress
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Adxtti\'s YTD Compressor";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
