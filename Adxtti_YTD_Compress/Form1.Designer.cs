090a288ca22d269b29a982156fa2262e85f8438b
neopallium <108044510+notneopallium@users.noreply.github.com>
2024-07-10 20:13:56 -0400

    feat; add .ytd openFileDialog to button1

--- Adxtti_YTD_Compress/Form1.Designer.cs
+++ Adxtti_YTD_Compress/Form1.Designer.cs
@@ -79,6 +79,7 @@ namespace Adxtti_YTD_Compress
             this.button1.TabIndex = 4;
             this.button1.Text = "CHOOSE .YTD FILES";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // checkBox1
             // 
--- Adxtti_YTD_Compress/Form1.cs
+++ Adxtti_YTD_Compress/Form1.cs
@@ -21,5 +21,19 @@ namespace Adxtti_YTD_Compress
         {
             this.Close();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Multiselect = true,
+                Filter = "YTD files (*.ytd)|*.ytd"
+            };
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string[] files = openFileDialog.FileNames;
+                label2.Text = $".YTD'S LOADED: {files.Length}";
+            }
+        }
     }
 }
